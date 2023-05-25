5eaa1099a62c4371347eb919cb9806280d568f8f
usuario <angelromerocarmona2004@gmail.com>
2023-05-25 13:52:57 +0200

    Ejercicio 2 completado correctamente

--- src/Ejercicios/BloqueA.java
+++ src/Ejercicios/BloqueA.java
@@ -8,17 +8,16 @@ public class BloqueA {
 	 * 1.1 Método que devuelva un número aleatorio entre dos pasados por argumentos
 	 * del método.
 	 */
-	public static  int numeroAleatorio(int numeroIntroducido1,int numeroIntroducido2) {
+	public static int numeroAleatorio(int numeroIntroducido1, int numeroIntroducido2) {
 
 		/* Creamos las variables */
 		int numeroAleatorio;
 
-		/*Hacemos el mathRandom para que de uno de esos dos valores*/
-			numeroAleatorio = (int) (Math.random() * (numeroIntroducido1 - numeroIntroducido2) + numeroIntroducido2);
-			
-			return numeroAleatorio;
+		/* Hacemos el mathRandom para que de uno de esos dos valores */
+		numeroAleatorio = (int) (Math.random() * (numeroIntroducido1 - numeroIntroducido2) + numeroIntroducido2);
 
-		}
+		return numeroAleatorio;
+
 	}
 
 	/*
@@ -28,48 +27,50 @@ public class BloqueA {
 	 * tiene 18 o más.
 	 */
 
-	/* Creando la clase Persona */
-	class Persona {
-		/* Creando atributos de persona */
-		private String nombre;
-		private int edad;
-
-		/* Creando constructor de persona */
-		public Persona(String nombre, int edad) {
-
-			this.nombre = nombre;
-			this.edad = edad;
-		}
-
-		/* getters and setters */
-
-		public String getNombre() {
-			return nombre;
-		}
-
-		public void setNombre(String nombre) {
-			this.nombre = nombre;
-		}
-
-		public int getEdad() {
-			return edad;
-		}
-
-		public void setEdad(int edad) {
-			this.edad = edad;
-		}
-
-		/* ToString */
-		@Override
-		public String toString() {
-			return "Persona [nombre=" + nombre + ", edad=" + edad + "]";
+	public static String mayor_o_menor(Persona unaPersona) {
+		if (unaPersona.edad >= 18) {
+			return "Es mayor de edad";
+		} else {
+			return "Es menor de edad";
 		}
 	}
+}
 
 
-	
-	public static void mayor_o_menor() {
-		
-		
+/* Creando la clase Persona */
+class Persona {
+	/* Creando atributos de persona */
+	String nombre;
+	int edad;
+
+	/* Creando constructor de persona */
+	public Persona(String nombre, int edad) {
+
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+
+	/* getters and setters */
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	/* ToString */
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", edad=" + edad + "]";
 	}
 }
