76ce9dcec2d61ab84874b881b3939db67bb431f0
usuario <angelromerocarmona2004@gmail.com>
2023-05-25 13:35:54 +0200

    Arreglo del ejercicio 1

--- src/Ejercicios/BloqueA.java
+++ src/Ejercicios/BloqueA.java
@@ -4,23 +4,15 @@ import java.util.Scanner;
 
 public class BloqueA {
 
+
 	/*
 	 * 1.1 Método que devuelva un número aleatorio entre dos pasados por argumentos
 	 * del método.
 	 */
-	public static void numeroAleatorio() {
-		Scanner teclado = new Scanner(System.in);
+	public int numeroAleatorio(int numeroIntroducido1,int numeroIntroducido2) {
 
 		/* Creamos las variables */
-		int numeroIntroducido1, numeroIntroducido2;
 		int numeroAleatorio;
-
-		/* Pedimos los números por teclado */
-		System.out.println("Introduceme un número: ");
-		numeroIntroducido1 = Integer.parseInt(teclado.nextLine());
-
-		System.out.println("Introduceme otro número: ");
-		numeroIntroducido2 = Integer.parseInt(teclado.nextLine());
 
 		/* Creamos un bucle para facilitar el método */
 		for (int i = 0; i < 1; i++) {
@@ -37,8 +29,56 @@ public class BloqueA {
 				numeroAleatorio = numeroIntroducido2;
 				break;
 			}
-			System.out.println("El número elegido es: " + numeroAleatorio);
+			return numeroAleatorio;
 
 		}
 	}
+
+	/*
+	 * 1.2 Diseñe un método que imprima los datos de una persona ingresados por
+	 * teclado e indicar si es mayor o menor de edad. El método recibe una clase
+	 * Persona que tiene nombre y edad; crea la clase necesaria. Es mayor de edad si
+	 * tiene 18 o más.
+	 */
+
+	/* Creando la clase Persona */
+	class Persona {
+		/* Creando atributos de persona */
+		private String nombre;
+		private int edad;
+
+		/* Creando constructor de persona */
+		public Persona(String nombre, int edad) {
+
+			this.nombre = nombre;
+			this.edad = edad;
+		}
+
+		/* getters and setters */
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public int getEdad() {
+			return edad;
+		}
+
+		public void setEdad(int edad) {
+			this.edad = edad;
+		}
+
+		/* ToString */
+		@Override
+		public String toString() {
+			return "Persona [nombre=" + nombre + ", edad=" + edad + "]";
+		}
+
+	}
+	
+	public static void
 }
